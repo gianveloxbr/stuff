ecf21affb10d8a5593f08a67d119e64c4beaef3c
Gian Michel <gianveloxsi@gmail.com>
2018-09-16 10:35:55 -0300

    ok
    ok

---
+++ testers.js
@@ -0,0 +1,8 @@
+function hideShow(){
+var x = document.getElementsByClassName("adicionais");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
